9e670201219e213931719619536a3455915515f9
__tinu <tinufarid@gmail.com>
2022-02-07 21:51:01 -0800

    Streams implementation

--- src/main/java/com/tinufarid/stocktwits_v3/StockTwistsKStream.java
+++ src/main/java/com/tinufarid/stocktwits_v3/StockTwistsKStream.java
@@ -1,17 +1,23 @@
 package com.tinufarid.stocktwits_v3;
 
+import jdk.nashorn.internal.parser.JSONParser;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+
 
 import java.util.Properties;
 
 public class StockTwistsKStream {
+
 
     private static final Logger logger = LogManager.getLogger();
 
@@ -19,34 +25,28 @@ public class StockTwistsKStream {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, AppConfigs.applicationID);
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, AppConfigs.bootstrapServers);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
-        StreamsBuilder builder = new StreamsBuilder();
-        KStream<String, String> KS0 = builder.stream(AppConfigs.posTopicName,
-                Consumed.with(AppSerdes.String(), AppSerdes.PosInvoice()));
 
-        KS0.filter((k, v) ->
-                        v.getDeliveryType().equalsIgnoreCase(AppConfigs.DELIVERY_TYPE_HOME_DELIVERY))
-                .to(AppConfigs.shipmentTopicName, Produced.with(AppSerdes.String(), AppSerdes.PosInvoice()));
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+        KStream<String,String> kStream = streamsBuilder.stream(AppConfigs.topicName);
+        //kStream.foreach((k,v) -> System.out.println(v));
 
-        KS0.filter((k, v) ->
-                        v.getCustomerType().equalsIgnoreCase(AppConfigs.CUSTOMER_TYPE_PRIME))
-                .mapValues(invoice -> RecordBuilder.getNotification(invoice))
-                .to(AppConfigs.notificationTopic, Produced.with(AppSerdes.String(), AppSerdes.Notification()));
 
-        KS0.mapValues(invoice -> RecordBuilder.getMaskedInvoice(invoice))
-                .flatMapValues(invoice -> RecordBuilder.getHadoopRecords(invoice))
-                .to(AppConfigs.hadoopTopic, Produced.with(AppSerdes.String(), AppSerdes.HadoopRecord()));
 
-        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+
+        KStream<String,String> kStream1 = kStream.filter((k,v) -> new JSONObject(v).getString("sentiment").equals("Bullish"));
+        //kStream1.foreach((k,v) -> System.out.println(v));
+
+
+        KStream<String,String> kStream2 = kStream.filter((k,v) -> new JSONObject(v).getString("sentiment").equals("Bearish"));
+        kStream2.foreach((k,v) -> System.out.println(  new JSONObject(v).getString("body")));
+
+        Topology topology = streamsBuilder.build();
+        KafkaStreams streams = new KafkaStreams(topology, props);
         streams.start();
-
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            logger.info("Stopping Stream");
-            streams.close();
-        }));
-
     }
-
 
 
 }
